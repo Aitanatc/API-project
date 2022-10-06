a8b2fc5ccc54b4f3b4a998bd2ef3691251e879d4
Aitanantc <aitanantc@gmail.com>
2022-10-05 22:31:13 -0500

    oauth with github

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'))
 router.use('/movies', require('./movies'))
+router.use('/auth', (req, res) => {
+    res.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    );
+});
 
-
+router.use('/oauth-callback', (req, res) => {
+    res.redirect(
+        "https://github.com/",
+    );
+     
+  })
 
 module.exports = router;
